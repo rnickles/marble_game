150ddbeb85b6f6011bbb9d7b1f32022e661fad5c
rnickles <robbynickles@berkeley.edu>
2022-05-30 16:02:09 -0700

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,62 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './styles.css';
+import Matter from 'matter-js';
+import * as THREE from 'three';
+import { TrackballControls } from "three/examples/jsm/controls/TrackballControls";
+import { init_level } from "./levels/level1";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// This function is called once the window loads and is responsible for setting up the game.
+function init() {
+  
+  // establish the renderer
+  var renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setPixelRatio(window.devicePixelRatio);
+  document.body.appendChild(renderer.domElement);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  // setup the camera
+  var camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 10000);
+  camera.position.x = -100;
+  camera.position.y = 200;
+  camera.position.z = 1600;
+
+  // setup the camera controls to accept input from a mouse or touch
+  THREE.MOUSE.PAN = 0; // set pan to the left click
+  THREE.MOUSE.ROTATE = 2;
+  const controls = new TrackballControls(camera, renderer.domElement);
+  controls.noRotate = true; // we don't want rotation
+
+  // create a scene-graph data structure
+  var scene = new THREE.Scene();
+
+  // create a Matter.js engine
+  var engine = Matter.Engine.create({render: {visible: false}});
+
+  // initialize the level and retrieve a list of rendered representations
+  let _bodies = init_level(engine, scene);
+
+  //
+  // THE GAME LOOP
+  //
+  function render() {
+
+    requestAnimationFrame(render);
+
+    // sychronize the rendered representation with the physical representation.
+    for (var j = 0; j < engine.world.bodies.length; j++) {
+      var b = engine.world.bodies[j].position;
+      _bodies[j].position.set(b.x, b.y, 0)
+    }
+
+    // update any changes to the view based on user input from either mouse or touch.
+    controls.update();
+
+    // update the screen
+    renderer.render(scene, camera);
+  }
+
+  render();
+}
+
+    // schedule to call init() once the page loads.
+window.addEventListener('load', init);
